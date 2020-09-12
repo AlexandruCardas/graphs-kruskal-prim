191d179aaa944c02cb9ed11d352af6bd26133b9a
Alexandru Cardas <c17504869@mytudublin.ie>
2020-09-12 12:08:30 +0100

    Change algorithms

--- src/main/java/com/alexandru/Algorithms/DataStructures/Edge.java
+++ src/main/java/com/alexandru/Algorithms/DataStructures/Edge.java
@@ -3,19 +3,19 @@ import com.alexandru.Algorithms.Utils.CommonFunctions;
 
 public class Edge extends CommonFunctions
 {
-	public int origin;
-	public int destination;
-	public int weight;
+    public int origin;
+    public int destination;
+    public int weight;
 
-	public Edge(int origin, int destination, int weight)
-	{
-		this.origin = origin;
-		this.destination = destination;
-		this.weight = weight;
-	}
+    public Edge(int origin, int destination, int weight)
+    {
+        this.origin = origin;
+        this.destination = destination;
+        this.weight = weight;
+    }
 
-	public void show()
-	{
-		System.out.print(toChar(origin) + "--" + toChar(destination) + "(" + weight + ")");
-	}
+    public void show()
+    {
+        System.out.print(toChar(origin) + "--" + toChar(destination) + "(" + weight + ")");
+    }
 }
--- src/main/java/com/alexandru/Algorithms/DataStructures/LinkedListQueue.java
+++ src/main/java/com/alexandru/Algorithms/DataStructures/LinkedListQueue.java
@@ -7,66 +7,65 @@ import com.alexandru.Algorithms.Utils.CommonFunctions;
 // class taken from previous labs
 public class LinkedListQueue extends CommonFunctions implements Queue
 {
-	private Node head;
-	private Node tail;
+    private Node head;
+    private Node tail;
 
-	public LinkedListQueue()
-	{
-		head = null;
-		tail = null;
-	}
+    public LinkedListQueue()
+    {
+        head = null;
+        tail = null;
+    }
 
-	// assume the queue is non-empty when this method is called, otherwise throw exception
-	public void enQueue(int x)
-	{
-		Node newNode = new Node();
-		newNode.vertex = x;
-		newNode.next = null;
+    // assume the queue is non-empty when this method is called, otherwise throw exception
+    public void enQueue(int x)
+    {
+        Node newNode = new Node();
+        newNode.vertex = x;
+        newNode.next = null;
 
-		if (isEmpty()) // case of empty list
-		{
-			head = newNode;
-		}
-		else // case of list not empty
-		{
-			tail.next = newNode;
-		}
+        if (isEmpty()) // case of empty list
+        {
+            head = newNode;
+        } else // case of list not empty
+        {
+            tail.next = newNode;
+        }
 
-		tail = newNode; // new node is now at the tail
-	}
+        tail = newNode; // new node is now at the tail
+    }
 
-	public int deQueue() throws MyExceptions
-	{
-		if (isEmpty())
-		{
-			throw new MyExceptions("Queue is empty");
-		}
-		int value = head.vertex;
+    public int deQueue() throws MyExceptions
+    {
+        if (isEmpty())
+        {
+            throw new MyExceptions("Queue is empty");
+        }
+        int value = head.vertex;
 
-		if (head == tail)
-		{
-			tail = null;
-		}
+        if (head == tail)
+        {
+            tail = null;
+        }
 
-		head = head.next;
-		return value;
-	}
+        head = head.next;
+        return value;
+    }
 
-	public boolean isEmpty()
-	{
-		return head == null;
-	}
+    public boolean isEmpty()
+    {
+        return head == null;
+    }
 
-	public void printQueue()
-	{
-		System.out.println("\nThe queue values are:\n");
+    public void printQueue()
+    {
+        System.out.println("\nThe queue values are:\n");
 
-		Node temp = head;
-		while (temp != null)
-		{
-			System.out.print(toChar(temp.vertex) + "  ");
-			temp = temp.next;
-		}
-		System.out.println("\n");
-	}
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.print(toChar(temp.vertex) + "  ");
+            temp = temp.next;
+        }
+        System.out.println("\n");
+    }
 }
